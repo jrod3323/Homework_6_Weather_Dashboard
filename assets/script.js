f68b057475b80a5a28f881ef3bebc80669dec9e1
Jared Ackley <jrod3323@gmail.com>
2020-11-16 14:56:33 -0600

    add initial ajax call, queryUrl, and pull city and temp

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ function pullData(event){
     //grabs city from Search box
     var city = $("#searchedCity").val();
     // api URL
-    var queryURL =  "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=4a4e7e38b950e4866fbd81200ca3d5cb";
+    var queryURL =  "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=4a4e7e38b950e4866fbd81200ca3d5cb";
     // Ajax call to API
     $.ajax({
     url: queryURL,
@@ -19,9 +19,12 @@ function pullData(event){
     }).then(function(response) {
     //log the response from the api
     console.log(response);
+    //grab city
+    var cityName = response.city.name;
+    console.log(cityName);
     // calculate the temperature (converted from Kelvin)
-    var temp = response.main.temp
-    var farTemp = (temp-273.15)*1.8 +32
-    console.log(farTemp)
+    var currentTemp = response.list[0].main.temp;
+    var currentFarTemp = ((currentTemp-273.15)*1.8 +32).toFixed(2);
+    console.log(currentFarTemp)
     });
 }
