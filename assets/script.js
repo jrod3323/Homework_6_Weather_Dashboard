b9ade721de8ca074c03f7c3dee779fdf61ff1f1e
Jared Ackley <jrod3323@gmail.com>
2020-11-16 18:39:36 -0600

    add some dynamic elements for 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -23,30 +23,34 @@ function pullData(event){
     }).then(function(response) {
     //log the response from the api
     console.log(response);
+    //Fill in Data for Main Result Container
     //fill city and state
-    $(".currentCity").text(`${cityState}`);
+    $(".currentCity").text(`${cityState} ${today}`);
 
     //grab quick pic
-    //******************************* 
+   
     var quickPic = response.list[0].weather[0].icon
     console.log(quickPic);
     var quickPicURL = `http://openweathermap.org/img/wn/${quickPic}@2x.png`
     console.log(quickPicURL)
     $(".quickPic").attr("src",quickPicURL)
-    //**************************************** */
+    
 
     // calculate the temperature (converted from Kelvin)
     var currentTemp = response.list[0].main.temp;
-    var currentFarTemp = ((currentTemp-273.15)*1.8 +32).toFixed(2);
-    console.log(currentFarTemp)
-    $(".currentTemp").text(currentFarTemp)
+    var currentFarTemp = (((currentTemp-273.15)*1.8 +32).toFixed(2));
+    console.log(currentFarTemp);
+    $(".currentTemp").text(currentFarTemp+" °F");
     //get current wind speed
     var currentWindSpeed = response.list[0].wind.speed;
-    $(".currentWindSpeed").text(currentWindSpeed);
-    console.log(currentWindSpeed)
+    $(".currentWindSpeed").text(currentWindSpeed+" MPH");
+    console.log(currentWindSpeed);
+    //get humidity
+    var currentHumidity = response.list[0].main.currentHumidity;
+    $(".currentHumidity").text(currentHumidity+"%");
     //get UV Index ** must get lat and long
     var lat = response.city.coord.lat;
-    console.log(lat)
+    console.log(lat);
     var lon = response.city.coord.lon;
     console.log(lon);
 
@@ -70,4 +74,40 @@ function pullData(event){
         }
         });
     });
+    // Fill In 5 Day Forecast
+    //////////////////////////
+    //Fill in Dates
+    for(var i=0;i<5;i++){
+        //loops through dates
+        var dateCalc = moment().add(i,"d");
+        var date = dateCalc.format("L");
+        //create card div
+        var card = $("<div>");
+        card.addClass("card col-2");
+        //create UL
+        var ul = $("<ul>");
+        ul.addClass("list-group list-group-flush");
+        //create date li element
+        var cardDate = $("<li>");
+        cardDate.text(date);
+        cardDate.addClass("list-group-item cardDate");
+        
+        ///////////////////////////////////////////////////////
+        //pull daily quickPic
+        var dailyQuickPic = response.list[(i*8)].weather.icon ;
+        var dailyQuickPicURL = `http://openweathermap.org/img/wn/${dailyQuickPic}@2x.png`
+        console.log(dailyQuickPicURL)
+        var dailyPic = $("<li>")
+        var dailyPicImg = $("<img>");
+        dailyPicImg.attr("src",dailyQuickPicURL);
+        dailyPic.append(dailyPicImg);
+        //append to Dom
+        $(".fiveDayCards").append(card);
+        card.append(ul);
+        ul.append(cardDate,dailyPic);
+        //////////////////////////////////////////////////////
+    }
+
 }
+
+
